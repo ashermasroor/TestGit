1b34c810e7280b0c1c4f5bcbab98358dbc669d66
ashermasroor <syed.asher.masroor@gmail.com>
2024-05-17 13:12:40 +0800

    tp 6

--- script.py
+++ script.py
@@ -1,4 +1,4 @@
-
+import os
 import pytube
 
 #                               UI
@@ -6,12 +6,12 @@ import pytube
 
 # root.geometry("1000x600")
 
-# root.mainloop( TP5)
+# root.mainloop( TP6)
 
 #                            End of UI
 
-url = str(input("Paste URL:"))
-yt  = pytube.YouTube(url)
+     url = str(input("Paste URL:"))
+     yt  = pytube.YouTube(url)
 
 vids = yt.streams.filter(file_extension='webm', type = 'audio')
 
